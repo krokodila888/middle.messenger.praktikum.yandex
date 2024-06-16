b8cb1fa771af7c9d5a9c331d6448cd55c4f3d447
krokodila888 <krokodila888@yandex.ru>
2024-06-17 00:34:02 +0300

    FIX: test fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  //root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'build'),
     rollupOptions: {
@@ -21,9 +20,6 @@ export default defineConfig({
         if (/\.css$/.test(name ?? '')) {
             return 'assets/css/[name]-[hash][extname]';   
         }
-  
-        // default value
-        // ref: https://rollupjs.org/guide/en/#outputassetfilenames
         return 'assets/[name]-[hash][extname]';
       },
     },
