e8b69eaa8e72b94aaa8036667721afc43d9fc666
krokodila888 <krokodila888@yandex.ru>
2024-07-27 19:17:50 +0300

    FEAT: avatar nearby

--- src/components/input_button/input-button.ts
+++ src/components/input_button/input-button.ts
@@ -69,21 +69,17 @@ export class InputButton extends Block {
           const form = document.querySelector('.fileinput-profile-field__input-raw');
           const formdata = new FormData(form as HTMLFormElement);
 
-
           const input = (<HTMLInputElement>document.querySelector('input[type="file"]'));
           if (input !== null && input instanceof HTMLInputElement && input.files) {
-            console.log ('file');
-            console.log()
+            console.log (input.files[0]);
+            console.log (input);
             formdata.append('file', input.files[0]);
+            console.log(formdata);
           }
           return new HTTPTransport()
             .put('https://ya-praktikum.tech/api/v2/user/profile/avatar', {
               credentials: 'include',
               mode: 'cors',
-              withCredentials: true,
-              headers: {
-                'Content-Type': 'multipart/form-data',
-              },
               body: formdata
             })
             .then((xhr) => {
