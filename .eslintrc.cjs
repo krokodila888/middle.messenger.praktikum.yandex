abc4eb17afee6d26d4c0dd04c1f53ac9d5b13a59
krokodila888 <krokodila888@yandex.ru>
2024-08-05 01:18:30 +0300

    FEAT: nearby

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,7 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: "module",
     warnOnUnsupportedTypeScriptVersion: false,
-    project: [`./tsconfig.json`],
-    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
   },
 
   settings: {
